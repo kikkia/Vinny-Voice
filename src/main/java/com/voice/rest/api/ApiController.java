b915157bb37a2b87b1cda5a1b48e6a36019dd1c8
JessWalters <jessw@iastate.edu>
2020-05-21 01:23:02 -0500

    add dislog

--- src/main/java/com/voice/rest/api/ApiController.java
+++ src/main/java/com/voice/rest/api/ApiController.java
@@ -1,13 +1,17 @@
 package com.voice.rest.api;
 
+import com.voice.utils.DislogLogger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
+    private DislogLogger logger = new DislogLogger(this.getClass());
+
     @RequestMapping("/")
     public String test() {
+        logger.info("ayyyy");
         return "ayyy";
     }
 }
